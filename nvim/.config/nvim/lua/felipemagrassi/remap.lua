418780d09e52c599d113e685f1fa8f2d5b390f5f
Felipe Magrassi <Felipe.1magrassi@gmail.com>
2023-04-25 22:46:52 -0300

    better remaps and lsp config

--- nvim/.config/nvim/lua/felipemagrassi/remap.lua
+++ nvim/.config/nvim/lua/felipemagrassi/remap.lua
@@ -1,5 +1,5 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Explore)
+vim.keymap.set("n", "<leader>e", vim.cmd.Explore)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
@@ -21,7 +21,6 @@ vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
@@ -31,4 +30,4 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
-vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.config/nvim/lua/felipemagrassi/packer.lua<CR>");
+vim.keymap.set("n", "<leader>vc", "<cmd>e ~/.config/nvim/lua/felipemagrassi/packer.lua<CR>");

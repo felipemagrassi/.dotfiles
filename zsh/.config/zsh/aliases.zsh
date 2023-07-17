61ad728ab376655e674e003b82ebe172a011a832
felipe <felipe@pop-os.localdomain>
2023-07-16 21:28:44 -0300

    alacritty and zsh

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -1,9 +1,3 @@
-
-gd() {
-  preview="git diff $@ --color=always -- {-1}"
-  git diff $@ --name-only | fzf -m --ansi --preview $preview
-}
-
 alias rebase='git fetch origin master:master; git rebase master'
 alias vim='nvim'
 alias lvim='$HOME/.local/bin/lvim'

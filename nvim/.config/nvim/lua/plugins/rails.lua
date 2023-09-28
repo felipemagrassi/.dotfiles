9be2e7b9c5afbdce6acf325ec0cb0e59b97a1e74
Felipe Magrassi <Felipe.1magrassi@gmail.com>
2023-09-28 00:39:58 -0300

    use kickstart nvim

--- nvim/.config/nvim/lua/plugins/rails.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "tpope/vim-rails",
-    ft = { "ruby" },
-    keys = {
-      { "<leader>ja", [[:execute "e " . eval('rails#buffer().alternate()')<cr>]], desc = "Jump rails alternate" },
-    },
-  },
-}

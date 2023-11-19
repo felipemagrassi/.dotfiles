d4b0d8d6ea64a2c6995f444b84c00cb15336d8b3
Felipe Magrassi <Felipe.1magrassi@gmail.com>
2023-11-18 23:10:41 -0300

    add-hard-moded

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -353,8 +353,6 @@ require('lazy').setup({
       require("catppuccin").setup({
         transparent_background = true
       })
-
-      vim.cmd [[colorscheme gruvbox]]
     end
   },
   {
@@ -832,6 +830,34 @@ require("headlines").setup {
   },
 }
 require 'colorizer'.setup()
+
+-- Default options:
+require("gruvbox").setup({
+  terminal_colors = true, -- add neovim terminal colors
+  undercurl = true,
+  underline = true,
+  bold = true,
+  italic = {
+    strings = true,
+    emphasis = true,
+    comments = true,
+    operators = false,
+    folds = true,
+  },
+  strikethrough = true,
+  invert_selection = false,
+  invert_signs = false,
+  invert_tabline = false,
+  invert_intend_guides = false,
+  inverse = true,    -- invert background for search, diffs, statuslines and errors
+  contrast = "hard", -- can be "hard", "soft" or empty string
+  palette_overrides = {},
+  overrides = {},
+  dim_inactive = false,
+  transparent_mode = false,
+})
+
+vim.cmd("colorscheme gruvbox")
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et

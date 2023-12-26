2f128ee05ad64183d608c9afd37124d8c037d44a
Felipe Magrassi <Felipe.1magrassi@gmail.com>
2023-12-25 22:17:33 -0300

    add wallpapers and rest.nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -84,6 +84,59 @@ require('lazy').setup({
   -- Unless you are still migrating, remove the deprecated commands from v1.x
   --
   { "norcalli/nvim-colorizer.lua" },
+  {
+    "rest-nvim/rest.nvim",
+    dependencies = { { "nvim-lua/plenary.nvim" } },
+    ft = "http",
+    keys = {
+      { "<leader>tr", "<Plug>RestNvim<cr>", desc = "Test Request" },
+    },
+    config = function()
+      require("rest-nvim").setup({
+        -- Open request results in a horizontal split
+        result_split_horizontal = false,
+        -- Keep the http file buffer above|left when split horizontal|vertical
+        result_split_in_place = false,
+        -- stay in current windows (.http file) or change to results window (default)
+        stay_in_current_window_after_split = false,
+        -- Skip SSL verification, useful for unknown certificates
+        skip_ssl_verification = false,
+        -- Encode URL before making request
+        encode_url = true,
+        -- Highlight request on run
+        highlight = {
+          enabled = true,
+          timeout = 150,
+        },
+        result = {
+          -- toggle showing URL, HTTP info, headers at top the of result window
+          show_url = true,
+          -- show the generated curl command in case you want to launch
+          -- the same request via the terminal (can be verbose)
+          show_curl_command = false,
+          show_http_info = true,
+          show_headers = true,
+          -- table of curl `--write-out` variables or false if disabled
+          -- for more granular control see Statistics Spec
+          show_statistics = false,
+          -- executables or functions for formatting response body [optional]
+          -- set them to false if you want to disable them
+          formatters = {
+            json = "jq",
+            html = function(body)
+              return vim.fn.system({ "tidy", "-i", "-q", "-" }, body)
+            end
+          },
+        },
+        -- Jump to request line on run
+        jump_to_request = false,
+        env_file = '.env',
+        custom_dynamic_variables = {},
+        yank_dry_run = true,
+        search_back = true,
+      })
+    end
+  },
 
   {
     "nvim-neo-tree/neo-tree.nvim",
@@ -858,6 +911,7 @@ require("gruvbox").setup({
 })
 
 vim.cmd("colorscheme gruvbox")
+vim.api.nvim_set_hl(0, "Normal", { guibg = NONE, ctermbg = NONE })
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -107,8 +107,26 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 export PATH=/home/felipe/.local/bin:$PATH
+export GOPATH=$HOME/go
+export PATH=$PATH:$GOPATH/bin
 
 source "$HOME/.config/zsh/exports.zsh"
 source "$HOME/.config/zsh/aliases.zsh"
 
 eval "$(starship init zsh)"
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/home/felipe/anaconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/home/felipe/anaconda3/etc/profile.d/conda.sh" ]; then
+        . "/home/felipe/anaconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/home/felipe/anaconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+

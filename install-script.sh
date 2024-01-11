1ea17c3462b846637b866504121904fb61ec4a3b
felipe magrassi <felipe.1magrassi@gmail.com>
2024-01-10 23:07:57 -0300

    add-python

--- install-script.sh
+++ install-script.sh
@@ -8,7 +8,8 @@ sudo apt-get install -y \
   git-core zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev \
   keychain \
   mpv  \
-  fzf
+  fzf \
+  lzma
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 curl -sS https://starship.rs/install.sh | sh
@@ -24,9 +25,12 @@ git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.13.1
 source ~/.zshrc
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
+asdf plugin add python https://github.com/asdf-community/asdf-python
 asdf install nodejs latest
 asdf install ruby latest
+asdf install python latest
 asdf global nodejs latest
 asdf global ruby latest
+asdf global python latest
 cd
 

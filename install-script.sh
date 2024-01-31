b3ab651c54a81c071cde08ec21097a2ffb2fec14
Felipe Magrassi <felipe.1magrassi@gmail.com>
2024-01-30 21:40:42 -0300

    relogith

--- install-script.sh
+++ install-script.sh
@@ -14,6 +14,16 @@ sudo apt-get install -y \
   fzf \
   lzma
 
+wget -qO - https://regolith-desktop.org/regolith.key | \
+gpg --dearmor | sudo tee /usr/share/keyrings/regolith-archive-keyring.gpg > /dev/null
+echo deb "[arch=amd64 signed-by=/usr/share/keyrings/regolith-archive-keyring.gpg] \
+https://regolith-desktop.org/release-3_0-ubuntu-jammy-amd64 jammy main" | \
+sudo tee /etc/apt/sources.list.d/regolith.list
+
+sudo apt update
+sudo apt install regolith-desktop regolith-session-flashback regolith-look-lascaille regolith-look-dracula
+sudo apt install regolith-desktop regolith-session-sway regolith-look-nord
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 

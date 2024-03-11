f0091cce2874b982403926a38f2f31a9f16e89d4
Felipe Magrassi <felipe.magrassi@useblu.com.br>
2024-03-11 09:24:35 -0300

    use rubocop

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -625,12 +625,12 @@ require('lazy').setup {
     opts = {
       notify_on_error = true,
       format_on_save = {
-        timeout_ms = 500,
+        timeout_ms = 5000,
         lsp_fallback = true,
       },
       formatters_by_ft = {
         lua = { 'stylua' },
-        ruby = { 'rubyfmt' },
+        ruby = { 'rubocop' },
         eruby = { 'erb-format' },
         go = { 'gofmt', 'goimports' },
         -- Conform can also run multiple formatters sequentially
